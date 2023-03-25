e928180248e2388c0f6029d4d881729a6434b933
Ksu <ksysha.2002r@gmail.com>
2023-03-25 18:38:56 +0300

    make sending review (finished) and start star review

--- scripts/add_review.js
+++ scripts/add_review.js
@@ -3,9 +3,9 @@ $("#button_send_review").click(function () {
     review = $("#review").val()
     getCounter()
     $.get("review.html", function (data) {
-        if ($("#new_review0").length) {
+        if (counter > 0) {
             var new_review = "#new_review" + (counter - 1)
-            $(new_review).html(data)
+            $(new_review).append(data)
             $(new_review).attr("id", "new_review" + counter)
         } else {
             $("#reviews").html(data)
@@ -13,38 +13,33 @@ $("#button_send_review").click(function () {
         }
     })
     setTimeout(function () {
-        $("#image_acc").attr("class", "img" + counter);
-        $("#name_acc").attr("class", "name" + counter);
-        $("#date_acc").attr("class", "date" + counter);
-        $("#review_acc").attr("class", "review_acc review" + counter);
-        /* var name_acc = $("#name_acc")
-         var counter = 0
-         var date_acc = $("#date_acc")
-         var review_acc = $("#review_acc")
-         var new_review = $("#new_review")
-         $('#review').each(function (index) {
-             counter++;
-             $(name_acc).attr("class", "name" + counter);
-             $(date_acc).attr("class", "date" + counter);
-             $(review_acc).attr("class", "review_acc review" + counter);
-             $(new_review).attr("id", "new_review" + counter)
-         })*/
+        var name_acc = $(".name")
+        var date_acc = $(".date")
+        var review_acc = $("#review_acc")
+        var image_acc = $(".img")
+        $(image_acc).attr("class", "img" + counter);
+        $(name_acc).attr("class", "name" + counter);
+        $(date_acc).attr("class", "date" + counter);
+        $(review_acc).attr("id", "review_acc" + counter);
         var img_source = "../../resources/img/tigers/masha.jpg"
         var img_name = ".img" + counter
         var str_name = ".name" + counter
         var str_date = ".date" + counter
-        var str_review = ".review" + counter
+        var str_review = "#review_acc" + counter
         $(img_name).attr("src", img_source)
         $(str_name).html("Машка")
         var date = new Date($.now())
         $(str_date).html(`${date.toDateString()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
         $(str_review).html(review)
-    }, 500)
+        for (let i = 0; i < 3; i++) {
+            $(".score-box_review").append("<div class = star </div>")
+        }
+    }, 1000)
 })
 
 function getCounter() {
     counter = 0
-    $('#published_review').each(function (index) {
+    $(".published_review").each(function (index) {
         counter++;
     })
 }
