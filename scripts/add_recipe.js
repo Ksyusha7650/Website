352435c3cc6d961c466f6230fcd0e69393cb8dc2
Ksu <ksysha.2002r@gmail.com>
2023-04-30 14:16:18 +0300

    make correct connection between ingredients and recipes 😴🥰🥳😴

--- scripts/add_recipe.js
+++ scripts/add_recipe.js
@@ -5,31 +5,31 @@ function apply() {
     var theme = $("#theme").val()
     var ingredients = $("#ingredients_textarea").val()
     send_recipe_to_db(dish_name, dish, theme)
-    send_ingredients_to_db(ingredients)
-    open("recipe_template.html", "_self")
-}
-
-function send_ingredients_to_db($ingredients) {
-    var array_of_ingredients = $ingredients.split('\n')
+    var array_of_ingredients = ingredients.split('\n')
     for (let index in array_of_ingredients) {
         var ing_line = array_of_ingredients[index].split('-')
         var name = ing_line[0]
         var amount = ing_line[1]
+        send_ingredients_to_db(name, amount)
+        setTimeout(this, 500)
+        send_ingredients_to_recipe()
+    }
+    open("recipe_template.html", "_self")
+}
+
+function send_ingredients_to_db($name, $amount) {
         $.ajax({
             type: "POST",
             url: "../php/add_ingredients.php",
-            data: {id_acc: localStorage.getItem("id_account"), ingredient_name: name, amount: amount},
+            data: {id_acc: localStorage.getItem("id_account"), ingredient_name: $name, amount: $amount},
         })
             .done(function (data) {
                 if (data === "Произошла ошибка при выполнении запроса") {
                     alert("Произошла ошибка!");
                 } else {
                     localStorage.setItem("id_ingredient", data);
-                    send_ingredients_to_recipe()
                 }
             });
-    }
-
 }
 
 function send_recipe_to_db($name, $dish, $theme) {
